316807c527cf03b0b6d10bac0e8146d9148f8261
Chen Pei An <3a832009@gm.student.ncut.edu.tw>
2021-11-15 23:42:19 +0800

    第二個路由:新增任務與資料驗證-錯誤訊息

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use  App\Models\Task;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,25 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('tasks');
+});
+// 增加新的任務
 Route::post('/task', function (Request $request) {
-    // 驗證輸入
-    $validator = Validator::make($request->all() , [
+    $validator = Validator::make($request->all(), [
         'name' => 'required|max:255',
     ]);
+
     if ($validator->fails()) {
         return redirect('/')
             ->withInput()
             ->withErrors($validator);
     }
+
     // 建立該任務...
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-    return redirect('/');
-});
-// 增加新的任務
-Route::post('/task', function (Request $request) {
-    //
 });
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task) {
