ca1f71d4333bac40306d7afaf97efec90d57e80e
Chen Pei An <3a832009@gm.student.ncut.edu.tw>
2021-11-15 23:36:17 +0800

    第二個路由:新增任務與資料驗證

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('tasks');
+Route::post('/task', function (Request $request) {
+    // 驗證輸入
+    $validator = Validator::make($request->all() , [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // 建立該任務...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
 });
 // 增加新的任務
 Route::post('/task', function (Request $request) {
